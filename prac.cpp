7b4e1626f0f734dea5f3caaa9d2eb149d8491e64
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2021-11-03 23:05:19 -0700

    Update prac.cpp

--- prac.cpp
+++ prac.cpp
@@ -11,13 +11,24 @@ char phrase[100];
 cin.get(phrase, 100);
 cin.get();
 cout << phrase << endl;
-//int len = strlen(phrase);
+int len = strlen(phrase);
 //int count = 0;
 
+for(int i = 0; i < len; i++){
+if(i == 0){
+  char temp = phrase[0];
+  phrase[0] = phrase[1];
+  phrase[1] = temp;
+}
+  
+if(phrase[i] == ' '){
+  char temp = phrase[i+1];
+  phrase[i+1] = phrase[i+2];
+  phrase[i+2] = temp;
+}
 
-char temp = phrase[0];
-phrase[0] = phrase[1];
-phrase[1] = temp;
+}
+
   
 cout << phrase << endl;
   
